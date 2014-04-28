48b4da9036277425b4d35df087adb5500ed06ab4
Eric Liao <rcliao01@gmail.com>
2014-04-27 21:26:50 -0700

    implement swipe function for getting menu out and in; include semantic ui library

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -12,9 +12,19 @@ angular.module('myApp.controllers', [])
 
 	$scope.showMenu = function() {
 		$('.ui.sidebar')
+			.sidebar('show')
+		;
+	};
+	$scope.closeMenu = function() {
+		$('.ui.sidebar')
+			.sidebar('hide')
+		;
+	};
+	$scope.toggleMenu = function() {
+		$('.ui.sidebar')
 			.sidebar('toggle')
 		;
-	}
+	};
   }])
   .controller('MyCtrl2', [function() {
 

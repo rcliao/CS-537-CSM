a4a62390f6519bad9a6683a8b437c0b5beaa859a
Eric Liao <rcliao01@gmail.com>
2014-04-27 23:12:26 -0700

    implement basic login form with button state

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,12 +3,15 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'ngTouch',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+	'ngRoute',
+	'ngTouch',
+	'ngAnimate',
+	'myApp.filters',
+	'myApp.services',
+	'myApp.directives',
+	'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+	$routeProvider.otherwise({redirectTo: '/login'});
 }]);
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -3,29 +3,49 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MainCtrl', ['$scope', function($scope) {
-  	$scope.init = function() {
-		$('.ui.sidebar')
-			.sidebar()
-		;
-	};
+	.controller('MainCtrl', ['$scope', function($scope) {
+	  	$scope.init = function() {
+			$('.ui.sidebar')
+				.sidebar()
+			;
+			$('.ui.dropdown')
+				.dropdown()
+			;
+		};
 
-	$scope.showMenu = function() {
-		$('.ui.sidebar')
-			.sidebar('show')
-		;
-	};
-	$scope.closeMenu = function() {
-		$('.ui.sidebar')
-			.sidebar('hide')
-		;
-	};
-	$scope.toggleMenu = function() {
-		$('.ui.sidebar')
-			.sidebar('toggle')
-		;
-	};
-  }])
-  .controller('MyCtrl2', [function() {
+		// functions related to the menu control
+		$scope.showMenu = function() {
+			$('.ui.sidebar')
+				.sidebar('show')
+			;
+		};
+		$scope.closeMenu = function() {
+			$('.ui.sidebar')
+				.sidebar('hide')
+			;
+		};
+		$scope.toggleMenu = function() {
+			$('.ui.sidebar')
+				.sidebar('toggle')
+			;
+		};
+	}])
+	.controller('LoginCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+		$scope.login = function() {
+			$scope.loading = true;
+			$http.get('/rest/login')
+				.success(function() {
+					$scope.loading = false;
+				})
+				.error(function() {
+					$scope.loading = false;
+					$scope.loginError = true;
 
-  }]);
+					$timeout($scope.setStatusBack, 2000);
+				});
+		};
+
+		$scope.setStatusBack = function() {
+			$scope.loginError = false;
+		};
+	}]);

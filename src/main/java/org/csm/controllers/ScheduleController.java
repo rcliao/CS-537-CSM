634855a25215ff08500f3551f8ade768ce716c44
samansaeedi <ssaeedi1367@gmail.com>
2014-05-24 16:29:11 -0700

    modified pom and schedule

--- src/main/java/org/csm/controllers/ScheduleController.java
+++ src/main/java/org/csm/controllers/ScheduleController.java
@@ -4,11 +4,15 @@ import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.csm.models.Schedule;
@@ -22,8 +26,23 @@ public class ScheduleController {
 	@Path("/{courseName}/{term}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Schedule> getSchedules(@PathParam("courseName") String courseName, 
-			@PathParam("term") String term){
+			@PathParam("term") String term, @Context HttpServletRequest request, 
+			@Context HttpServletResponse response){
 		ScheduleDao schedule = new ScheduleDaoImpl();
+		HttpSession session = request.getSession(true);
+		if(term == null || term.isEmpty())
+		{
+			Calendar c = Calendar.getInstance();
+			int month = c.get(Calendar.MONTH);
+			if(month < 4)
+				term = "winter ";
+			else if(month < 7)
+				term = "spring ";
+			else if (month < 10)
+				term = "summer ";
+			else term = "fall ";
+			term += c.get(Calendar.YEAR);
+		}
 		try {
 			return schedule.getSchedules(courseName, term);
 		} catch (SQLException e) {
@@ -36,7 +55,8 @@ public class ScheduleController {
 	@Path("/{courseName}/{term}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Schedule> getSchedules(@PathParam("courseName") String courseName, 
-			@PathParam("term") String term, @QueryParam("available") boolean available){
+			@PathParam("term") String term, @QueryParam("available") boolean available, @Context HttpServletRequest request, 
+			@Context HttpServletResponse response){
 		ScheduleDao schedule = new ScheduleDaoImpl();
 		if(term == null || term.isEmpty())
 		{

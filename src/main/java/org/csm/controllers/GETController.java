f860b428c4f3a4fb129b2a79bd44c91ca691bd3d
samansaeedi <ssaeedi1367@gmail.com>
2014-05-24 17:39:21 -0700

    implemented enroll and unenroll. fixed schedule

--- src/main/java/org/csm/controllers/GETController.java
+++ src/main/java/org/csm/controllers/GETController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,11 +21,10 @@ import org.csm.models.User;
 import org.csm.models.dao.ScheduleDao;
 import org.csm.models.dao.jdbc.ScheduleDaoImpl;
 
-@Path("/schedules")
-public class ScheduleController {
-
+@Path("/GET")
+public class GETController {
 	@POST
-	@Path("/{courseName}/{term}")
+	@Path("/schedules/{courseName}/{term}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Schedule> getSchedules(
 			@PathParam("courseName") String courseName,
@@ -59,9 +59,8 @@ public class ScheduleController {
 		}
 		return null;
 	}
-
 	@POST
-	@Path("/{courseName}/{term}")
+	@Path("/schedules/{courseName}/{term}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Schedule> getSchedules(
 			@PathParam("courseName") String courseName,
@@ -95,6 +94,42 @@ public class ScheduleController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
+	}	
+	@POST
+	@Path("/enroll/{scheduleId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String enroll(@PathParam("courseId") Integer scheduleId, @Context HttpServletRequest request,
+			@Context HttpServletResponse response){
+		ScheduleDao scheduleDao = new ScheduleDaoImpl();
+		HttpSession session = request.getSession(true);
+		User u = (User) session.getAttribute("loginUser");
+		if (u == null) {
+			response.setStatus(401);
+			return null;
+		}
+		else{
+			scheduleDao.enroll(u, scheduleId);
+		}
+		response.setStatus(201);
+		return null;
+	}
+	@DELETE
+	@Path("/enroll/{scheduleId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String UnEnroll(@PathParam("courseId") Integer scheduleId, @Context HttpServletRequest request,
+			@Context HttpServletResponse response){
+		ScheduleDao scheduleDao = new ScheduleDaoImpl();
+		HttpSession session = request.getSession(true);
+		User u = (User) session.getAttribute("loginUser");
+		if (u == null) {
+			response.setStatus(401);
+			return null;
+		}
+		else{
+			scheduleDao.unEnroll(u, scheduleId);
+		}
+		response.setStatus(201);
 		return null;
 	}
 }

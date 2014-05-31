c26ab432b52e5ea1cb97864c8d4a7adb181c035c
Elnaz <bayazian.elnaz@gmail.com>
2014-05-30 22:48:41 -0700

    implement unit test

--- src/main/webapp/test/unit/controllersSpec.js
+++ src/main/webapp/test/unit/controllersSpec.js
@@ -2,23 +2,32 @@
 
 /* jasmine specs for controllers go here */
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(angular.mock.module('myApp.controllers'));
 
-
-  it('should have MainCtrl', inject(function($controller) {
-    //spec body
-    var mainCtrl = $controller('MainCtrl');
+  it('should have MainCtrl',function(){
+    var mainCtrl =$controllers('MainCtrl');
     expect(mainCtrl).toBeDefined();
-  }));
-
-  it('should GETCtrl' inject(function($controller) {
-    //spec body
-    var getCtrl = $controller('getCtrl');
-    expect(getCtrl).toBeDefined();
-  }));
+  });
 });
 
-describe('GETCtrl', function() {
-  // set up the controllers
-  beforeEach(module('myApp.controllers'));
+describe('play_ground', function() {
+  it('1 should be 1', function(){
+    expect(true).toBe(true);
+  });
+
+  it('1+1 should be 2' , function(){
+    expect(1+1).toBe(2);
+  });
+
+  it('test veriable a is 1' , function(){
+    var a=1;
+
+    expect(a).toBe(1);
+  });
+
+  it('test variable a is not 0' , function() {
+    var a=1;
+
+    expect(1).not.toBe(0);
+  });
 });
